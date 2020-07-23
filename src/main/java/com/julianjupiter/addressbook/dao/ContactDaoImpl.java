506ef43cd38f74981ad4f4f0bd234390555051ea
julianjupiter <julianjupiter.io@gmail.com>
2020-07-24 02:39:21 +0800

    Refactor to create EntityManager instance for every transaction.

--- src/main/java/com/julianjupiter/addressbook/dao/ContactDaoImpl.java
+++ src/main/java/com/julianjupiter/addressbook/dao/ContactDaoImpl.java
@@ -3,22 +3,16 @@ package com.julianjupiter.addressbook.dao;
 import com.julianjupiter.addressbook.entity.Contact;
 import com.julianjupiter.addressbook.util.PersistenceManager;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
 class ContactDaoImpl implements ContactDao {
-    private final EntityManager entityManager;
-    private EntityTransaction transaction;
-
-    ContactDaoImpl() {
-        this.entityManager = PersistenceManager.entityManager();
-    }
 
     @Override
     public List<Contact> findAll() {
+        var entityManager = PersistenceManager.entityManager();
         List<Contact> contacts;
 
         try {
@@ -26,7 +20,6 @@ class ContactDaoImpl implements ContactDao {
             contacts = contactTypedQuery.getResultList();
         } finally {
             entityManager.close();
-            PersistenceManager.close();
         }
 
         return contacts;
@@ -34,13 +27,13 @@ class ContactDaoImpl implements ContactDao {
 
     @Override
     public Optional<Contact> findById(Long id) {
+        var entityManager = PersistenceManager.entityManager();
         Contact contact;
 
         try {
             contact = entityManager.find(Contact.class, id);
         } finally {
             entityManager.close();
-            PersistenceManager.close();
         }
 
         return Optional.ofNullable(contact);
@@ -48,16 +41,20 @@ class ContactDaoImpl implements ContactDao {
 
     @Override
     public void save(Contact contact) {
+        var entityManager = PersistenceManager.entityManager();
+        EntityTransaction transaction = null;
+
         try {
             transaction = entityManager.getTransaction();
             transaction.begin();
             entityManager.persist(contact);
             transaction.commit();
         } catch (Exception exception) {
-            transaction.rollback();
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
         } finally {
             entityManager.close();
-            PersistenceManager.close();
         }
     }
 
@@ -65,16 +62,20 @@ class ContactDaoImpl implements ContactDao {
     public void deleteById(Long id) {
         Optional<Contact> contactOptional = findById(id);
         contactOptional.ifPresent(contact -> {
+            var entityManager = PersistenceManager.entityManager();
+            EntityTransaction transaction = null;
+
             try {
                 transaction = entityManager.getTransaction();
                 transaction.begin();
                 entityManager.remove(contact);
                 transaction.commit();
             } catch (Exception exception) {
-                transaction.rollback();
+                if (transaction != null && transaction.isActive()) {
+                    transaction.rollback();
+                }
             } finally {
                 entityManager.close();
-                PersistenceManager.close();
             }
         });
     }
